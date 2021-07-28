9be5a53fbf8b9b665a82445105a17322609b8270
Lio Young <lio@werewolf.design>
2021-07-28 09:35:40 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export default class Modulus {
+    url: string;
+
+    constructor(url: string) {
+        this.url = url;
+    }
+
+    public async server(): Promise<{}> {
+        // Get Data from Cultum Server
+
+        return Promise.resolve({})
+    }
+
+}
